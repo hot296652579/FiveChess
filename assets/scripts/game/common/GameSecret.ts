b2d351833748673594225383845373e5330aad5a
super_javan <296652579@qq.com>
2024-05-29 14:43:44 +0800

    cocos客户端添加 jsencrypt加密库

---
+++ assets/scripts/game/common/GameSecret.ts
@@ -0,0 +1,31 @@
+/*
+ * @Author: super_javan 296652579@qq.com
+ * @Date: 2024-05-29 14:29:32
+ * @LastEditors: super_javan 296652579@qq.com
+ * @LastEditTime: 2024-05-29 14:32:42
+ * @FilePath: /FiveChess/assets/scripts/game/common/GameSecret.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export const JsCrypt_PrivateKey = `-----BEGIN PRIVATE KEY-----
+MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALFe0bdivosU3hI5
+uTcKxn7v94rcJB1YIBogcPH0GQmq9RqyuOQj7suZm4vhHtiB4oe/Erm6xYmu1kuw
+XAS5ccJsBq0kmfnhW4QylHrFSVq+dK5zlWkEBPNn689yfHxqddVS3z5NeCG7eLkp
+ETvWbVl0/p/6tqRrFZHPeYc50wtfAgMBAAECgYEAhYrtfRRkgBTrCm/Zip6bSrFk
+AHG04z8ZlHI+d30gaM4Fkt9jI6oGPdbebRfHAJ3yL+Blud7sobg4cm5LKedxxP2Y
+lgfWny5Q66eNb6FXuPv/Pb1vJBiFulwgUv/+UTwVnRwF4ep9Q2Q3KU2VjUYmj/of
+Un/IwucoxqCuBzgzKsECQQDlCMqZGo5a6WXUQyDwdbDokXAbs7FldeI4Wveex73a
+FnQHNnvNaKa5k6ugsOcc9+rmbaQtZV6Nb96bc1dJWeS/AkEAxkDZwlapYP88sNsf
+M3JmKEEBa5ZF9rzqZlgaxLCXuSRbwFNg2gm7jQyNjAjikhTSfjzUBhR1cmt2wOKE
+46lhYQJAWVmPqCD7Di5lgmRZ0lUnmeM7KCN2/kz53uBmH7RutUjzJOk1bRH5kQYx
++3NoBh4VrqVxWiBkvaq53k0l7UC7gwJBAIr9SC1hCFqCVpA+7v7ogwpRmKdOsTos
+c8DQn+OpUpVwEgsh6VKzZBOc61dzmzSC+JecGaExkq1m/dh4dmYNh8ECQFzJw9Bv
+1TWZ0T3sPJUKJ5eQP5Wuevoi9JEaoVC/opBMIEaC8Nqz/aBVc35phNsXqJAdlA/+
+8/vpQmNmPhkx8jM=
+-----END PRIVATE KEY-----`;
+
+export const JsCrypt_PublicKey = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCxXtG3Yr6LFN4SObk3CsZ+7/eK
+3CQdWCAaIHDx9BkJqvUasrjkI+7LmZuL4R7YgeKHvxK5usWJrtZLsFwEuXHCbAat
+JJn54VuEMpR6xUlavnSuc5VpBATzZ+vPcnx8anXVUt8+TXghu3i5KRE71m1ZdP6f
++rakaxWRz3mHOdMLXwIDAQAB
+-----END PUBLIC KEY-----`;
